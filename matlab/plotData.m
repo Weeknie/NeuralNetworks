1379033f0885f5afc18dd51e97c211ee4b6ec7b1
Maarten Slenter <maartenslenter@gmail.com>
2013-02-01 20:47:35 +0100

    Added some extra plots for correct solution percentages and non converging percentages and labels

--- matlab/plotData.m
+++ matlab/plotData.m
@@ -1,2 +1,19 @@
-data = output(:,3)./(output(:,1) + output(:, 2));
+labelx = 'Population Size';
+
+subplot(2, 2, [1 2]);
+data = output(:, 1)./(output(:, 1) + output(:, 2)) * 100;
 plot(popSizes, data)
+xlabel(labelx);
+ylabel('% Correct');
+
+subplot(2, 2, 3);
+data = output(:, 2)./(output(:, 1) + output(:, 2)) * 100;
+plot(popSizes, data)
+xlabel(labelx);
+ylabel('% Non Converging');
+
+subplot(2, 2, 4);
+data = output(:, 3)./(output(:, 1) + output(:, 2));
+plot(popSizes, data)
+xlabel(labelx);
+ylabel('Average generations');
